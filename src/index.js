724d3f9a88f9d7a7a8c5af63cd6e183aa57edf80
Viktoriya Sim <simvvit@gmail.com>
2021-03-15 19:37:46 +0300

    feat: implement reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let absol=Math.abs(n);
+    let str=String(absol);
+    let mass=str.split("");
+    let rev=mass.reverse().join("");
+    let revred=Number(rev);
+    return revred;
 }
